35991757284da3a9a6e95a635b66a497b066e6e9
Yenifer Hernandez <paola.hernandeza@pragma.com.co>
2026-01-29 11:04:22 -0500

    initial commit

---
+++ src/test/java/com/bootcamp/capabilityservice/AbilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.capabilityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
